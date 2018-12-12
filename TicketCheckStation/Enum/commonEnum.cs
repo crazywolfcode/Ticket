8e4134e586e46dc898826c536505dbe7922b63f3
crazywolfcode <443055589@qq.com>
2018-12-12 21:02:15 +0800

    引入基础内容

---
+++ TicketCheckStation/Enum/commonEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketCheckStation
+{
+    public enum ConfigItemName
+    {
+        //connectionStrings
+        appSettings,
+        connectionStrings,
+        mysqlConn,
+        //appSettings
+        programVersion,
+        coryRight,
+        dbType,
+    }
+
+}
