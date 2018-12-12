3ee4de6a19532d4e6fb684258d930fca899c8e81
crazywolfcode <443055589@qq.com>
2018-12-12 23:28:12 +0800

    添加表和列的枚举

--- TicketCheckStation/Enum/TableName.cs
+++ TicketCheckStation/Enum/TableName.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TicketCheckStation
 {
+   /// <summary>
+   /// 所有表名
+   /// </summary>
     public enum TableName
     {
         bill_image,
@@ -22,5 +25,332 @@ namespace TicketCheckStation
         table_sync,
         user,
         weighing_bill,
+    }
+
+    public enum BillImageColumns
+    {
+        id,
+        positon,
+        address,
+        bill_id,
+        bill_number,
+        type,
+        is_up,
+        up_datetime,
+        remark,
+        add_time,
+        status,
+        add_user_id,
+        add_user_name,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        station_id,
+        station_name,
+    }
+    /// <summary>
+    ///车辆信息
+    /// </summary>
+    public enum CarInfoColumns
+    {
+        id,
+        car_number,
+        driver,
+        driver_mobile,
+        driver_idnumber,
+        owner_id,
+        owner_name,
+        remark,
+        add_time,
+        status,
+        add_user_id,
+        add_user_name,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+    }
+
+    /// <summary>
+    ///车辆皮重调整记录表
+    /// </summary>
+    public enum CarTraeRecodColumns
+    {
+        id,
+        car_number,
+        weight,
+        start_time,
+        end_time,
+        operator_id,
+        operator_name,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+
+    /// <summary>
+    ///公司信息
+    /// </summary>
+    public enum CompanyColumns
+    {
+        id,
+        name,
+        legal_person,
+        name_first_case,
+        credit_number,
+        businese_lincense_number,
+        licese_esprise_time,
+        abbreviation,
+        abbreviation_first_case,
+        level,
+        parent_id,
+        type,
+        address,
+        is_use_system,
+        regester_type,
+        customer_type,
+        affiliated_province_id,
+        affiliated_province,
+        remark,
+        add_time,
+        status,
+        add_user_id,
+        add_user_name,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+    }
+
+    /// <summary>
+    ///配置表
+    /// </summary>
+    public enum ConfigColumns
+    {
+        id,
+        config_name,
+        config_value,
+        config_type,
+        description,
+        station_id,
+        station_name,
+        remark,
+        add_time,
+        status,
+        add_user_id,
+        add_user_name,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+    }
+    /// <summary>
+    ///物质信息表
+    /// </summary>
+    public enum MaterialColumns
+    {
+        id,
+        cate_id,
+        cate_name,
+        name,
+        name_first_case,
+        curr_taxation,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+    /// <summary>
+    ///物质分类表
+    /// </summary>
+    public enum MaterialCategoryColumns
+    {
+        id,
+        name,
+        name_first_case,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+    /// <summary>
+    ///物价税价调整记录表
+    /// </summary>
+    public enum MaterialTaxationRecodColumns
+    {
+        id,
+        material_id,
+        material_name,
+        start_time,
+        end_time,
+        operator_id,
+        operator_name,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+    
+    /// <summary>
+    ///角色表
+    /// </summary>
+    public enum RolesColumns
+    {
+        id,
+        name,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+    /// <summary>
+    ///站点信息表
+    /// </summary>
+    public enum StationColumns
+    {
+        id,
+        name,
+        name_first_case,
+        privence,
+        city,
+        country,
+        detail_address,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+    /// <summary>
+    ///数据同步信息表
+    /// </summary>
+    public enum TableSyncColumns
+    {
+        id,
+        table_name,
+        sync_time,
+        sync_count,
+        has_up_status,
+        no_sync,
+        has_more,
+        limit_count,
+        is_commom,
+    }
+    /// <summary>
+    ///人员信息表
+    /// </summary>
+
+    public enum UserColumns
+    {
+        id,
+        name,
+        name_first_case,
+        id_number,
+        phone,
+        birthday,
+        sex,
+        role_id,
+        role_name,
+        pwd,
+        station_level_type,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
+    }
+    /// <summary>
+    ///过磅单
+    /// </summary>
+    public enum WeighingBillColumns
+    {
+        id,
+        station_id,
+        station_name,
+        send_company,
+        send_company_case,
+        receive_company,
+        material_cate_id,
+        receive_company_case,
+        material_cate_name,
+        material_cate_case,
+        material_id,
+        material_name,
+        material_first_case,
+        material_taxation,
+        car_number,
+        driver,
+        driver_id_umber,
+        phone,
+        send_gross_weight,
+        send_trae_weight,
+        send_net_weight,
+        car_trae_weight,
+        limit_weight,
+        overtop_weight,
+        overtop_money,
+        is_receive_money,
+        operator_id,
+        operator_name,
+        check_user_id,
+        check_user_name,
+        is_up,
+        up_datetime,
+        remark,
+        add_time,
+        last_update_time,
+        is_delete,
+        delete_time,
+        last_update_user_id,
+        last_update_user_name,
+        status,
+        add_user_id,
+        add_user_name,
     }
 }
