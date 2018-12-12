d91d6147bbefef02517dffd04313e871cddcb229
crazywolfcode <443055589@qq.com>
2018-12-12 21:53:46 +0800

    删除多余的内容

--- TicketCheckStation/ui/MainWindow.xaml.cs
+++ TicketCheckStation/ui/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace TicketCheckStation
     {
         public MainWindow()
         {
-            InitializeComponent();           
+            InitializeComponent();         
         }
     }
 }
